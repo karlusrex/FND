6f7f176ff6713bdd49dd33a8f504573c7f23c7ae
itzhapp <90254802+karlusrex@users.noreply.github.com>
2023-12-21 19:08:47 +0100

    refactor: relocate web folder and showcase flask svelte integration
    move web folder
    showcase flask svelte integration through /api route

---
+++ web/src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+
+  import { onMount } from 'svelte';
+
+  let data: string;
+
+  onMount(async () => {
+    const response = await fetch('/api/getRandom');
+    data = await response.text();
+  });
+</script>
+
+<main>
+  <h1>Flask + Nginx + Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>Random number: {data}</p>
+</main>
